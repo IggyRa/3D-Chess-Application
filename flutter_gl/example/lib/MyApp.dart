ec3e541ac7d0724090419b07a8a273eec5f5a05e
Igor Raczkiewicz <01158965@pw.edu.pl>
2024-06-28 16:22:46 +0200

    Adding initial files

---
+++ flutter_gl/example/lib/MyApp.dart
@@ -0,0 +1,24 @@
+//import 'dart:ui' as ui;
+import 'package:flutter/material.dart';
+// import 'package:flutter_gl_example/ExampleDemoTest.dart';
+//import 'ExampleTriangle01.dart';
+import 'ExampleCube.dart';
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    //ExampleTriangle01  only fix for web if you want run on native app maybe need fix shader???
+    //return ExampleTriangle01();
+    return ExampleCube();
+  }
+}
